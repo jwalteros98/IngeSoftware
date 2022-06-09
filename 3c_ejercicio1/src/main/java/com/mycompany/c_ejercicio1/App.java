8f8e8055986c9f20a950312a4958e2bd4408010b
Usuario <Usuario@DESKTOP-P3OGUBB>
2022-06-09 15:18:24 -0500

    Updated

--- 3c_ejercicio1/src/main/java/com/mycompany/c_ejercicio1/App.java
+++ 3c_ejercicio1/src/main/java/com/mycompany/c_ejercicio1/App.java
@@ -14,6 +14,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Version 1");
-        System.out.println("Version 2");
     }
 }
